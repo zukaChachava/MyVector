3ba34fbdff24de44d5129e3e7499d66e82a4ae6d
zukaChachava <zurabi.chachava.1@btu.edu.ge>
2022-03-01 12:03:26 +0400

    Basic functionality done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include "vector.h"
+
+struct ZPoint{
+    int x, y;
+
+    ZPoint() {}
+
+    ZPoint(int x_point, int y_point): x(x_point), y(y_point){
+
+    }
+
+    ZPoint(ZPoint& point){
+        std::cout << "Copied" << std::endl;
+    }
+
+    void Print() const{
+        std::cout << "X -> " << x << " y -> " << y << std::endl;
+    }
+
+    ZPoint& operator=(const ZPoint& point){
+        std::cout << "Copy" << std::endl;
+        x = point.x;
+        y = point.y;
+        return *this;
+    }
+
+    ZPoint& operator=(ZPoint&& point){
+        std::cout << "Move" << std::endl;
+        x = point.x;
+        y = point.y;
+        return *this;
+    }
+
+    ~ZPoint(){
+        std::cout << "Destroyed" << std::endl;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    MyVector<ZPoint> points;
+    for(int i = 0; i < 10; i++)
+        points.PushBack(ZPoint(i, i + 1));
+
+    for(int i = 0; i < points.Size(); i++)
+        points[i].Print();
+
     return 0;
 }
--- vector.h
+++ vector.h
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#define INCREMENT 2
+
 template<typename T>
 class MyVector{
 private:
@@ -7,19 +9,48 @@ private:
     std::size_t _size;
     std::size_t _capacity;
 public:
-    MyVector()
-    {
+    MyVector(){
         _size = 0;
         _capacity = 4;
         _data = new T[_capacity];
     }
 
+    size_t Size() const{
+        return _size;
+    }
+
+    void PushBack(const T& value){
+        if(_size == _capacity)
+            Resize();
+        _data[_size] = value;
+        _size++;
+    }
+
+    const T& operator[](size_t index) const{
+        std::cout << "Const" << std::endl;
+        if(index >= _size)
+            throw std::invalid_argument("Out of scope exception");
+        return _data[index];
+    }
+
+    T& operator[](size_t index){
+        if(index >= _size)
+            throw std::invalid_argument("Out of scope exception");
+        return _data[index];
+    }
+
 private:
-    void resize(){
-        T* new_data = new T[_capacity * 2];
+    void Resize(){
+        T* new_data = new T[_capacity * INCREMENT];
         for(int i = 0; i < _size; i++)
             new_data[i] = _data[i];
         delete[] _data;
         _data = new_data;
+        _capacity *= 2;
+    }
+
+public:
+    ~MyVector(){
+        delete[] _data;
     }
 };
