594cad1759b4308b829044103ae1b9c76e8022df
zukaChachava <zurabi.chachava.1@btu.edu.ge>
2022-03-01 18:18:01 +0400

    Improvements

--- main.cpp
+++ main.cpp
@@ -2,16 +2,19 @@
 #include "vector.h"
 
 struct ZPoint{
-    int x, y;
+    int index, x, y;
 
     ZPoint() {}
 
-    ZPoint(int x_point, int y_point): x(x_point), y(y_point){
-
+    ZPoint(int index, int x_point, int y_point): index(std::move(index)), x(std::move(x_point)), y(std::move(y_point)){
+        std::cout << "Create with parameters index -> " << index << std::endl;
     }
 
     ZPoint(ZPoint& point){
         std::cout << "Copied" << std::endl;
+        x = point.x;
+        y = point.y;
+        index = point.index;
     }
 
     void Print() const{
@@ -22,25 +25,43 @@ struct ZPoint{
         std::cout << "Copy" << std::endl;
         x = point.x;
         y = point.y;
+        index = point.index;
         return *this;
     }
 
-    ZPoint& operator=(ZPoint&& point){
+    ZPoint& operator=(ZPoint&& point) noexcept {
         std::cout << "Move" << std::endl;
         x = point.x;
         y = point.y;
+        index = point.index;
         return *this;
     }
 
     ~ZPoint(){
-        std::cout << "Destroyed" << std::endl;
+        std::cout << "Destroyed index "<< index << std::endl;
     }
 };
 
 int main() {
     MyVector<ZPoint> points;
-    for(int i = 0; i < 10; i++)
-        points.PushBack(ZPoint(i, i + 1));
+
+    //Move
+    for(int i = 1; i < 3; i++){
+        std::cout << "LOOP ---- " << std::endl;
+        points.EmplaceBack(i, i, i + 1);
+        std::cout << "LOOP ---- END\n" << std::endl;
+    }
+    std::cout << "------------" << std::endl;
+
+    // Copy
+    ZPoint point(100, 5, 5);
+    points.PushBack(point);
+
+    std::cout << "------------" << std::endl;
+
+    std::cout << "Clearing . . . " << std::endl;
+    points.Clear();
+    std::cout << "Cleared " << std::endl;
 
     for(int i = 0; i < points.Size(); i++)
         points[i].Print();
--- vector.h
+++ vector.h
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #define INCREMENT 2
+#define CAPACITY 4
 
 template<typename T>
 class MyVector{
@@ -8,22 +9,16 @@ private:
     T* _data = nullptr;
     std::size_t _size;
     std::size_t _capacity;
+
 public:
     MyVector(){
         _size = 0;
-        _capacity = 4;
-        _data = new T[_capacity];
+        _capacity = CAPACITY;
+        _data = new T[CAPACITY];
     }
 
     size_t Size() const{
         return _size;
-    }
-
-    void PushBack(const T& value){
-        if(_size == _capacity)
-            Resize();
-        _data[_size] = value;
-        _size++;
     }
 
     const T& operator[](size_t index) const{
@@ -39,11 +34,46 @@ public:
         return _data[index];
     }
 
+    void PushBack(const T& value){
+        if(_size == _capacity)
+            Resize();
+        _data[_size++] = value;
+    }
+
+    void PushBack(T&& value){
+        if(_size == _capacity)
+            Resize();
+        _data[_size++] = std::move(value);
+    }
+
+    template<typename... Args>
+    T& EmplaceBack(Args&&... args){
+        if(_size == _capacity)
+            Resize();
+
+        //_data[_size] = T(std::forward<Args>(args)...); // need move
+        new(&_data[_size]) T(std::forward<Args>(args)...); // does not need move
+        return _data[_size++];
+    }
+
+    void PopBack(){
+        if(_size == 0)
+            return;
+        _size--;
+        _data[_size].~T();
+    }
+
+    void Clear(){
+        delete[] _data;
+        _size = 0;
+        _data = new T[4];
+    }
+
 private:
     void Resize(){
         T* new_data = new T[_capacity * INCREMENT];
         for(int i = 0; i < _size; i++)
-            new_data[i] = _data[i];
+            new_data[i] = std::move(_data[i]);
         delete[] _data;
         _data = new_data;
         _capacity *= 2;
